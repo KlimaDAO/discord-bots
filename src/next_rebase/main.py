ed787e2d544e5b518f5007f1690ccf486f92ba55
Marcus Aurelius <marcus_aurelius_0x@protonmail.com>
2022-03-05 19:38:36 -0500

    Remove unnecessary argument

--- src/next_rebase/main.py
+++ src/next_rebase/main.py
@@ -29,10 +29,7 @@ last_rebase_alert = 0
 
 
 def get_webhook():
-    return discord.Webhook.from_url(
-        os.environ["DISCORD_REBASE_BOT_WEBHOOK_URL"],
-        adapter=discord.webhook.sync.WebhookAdapter()
-    )
+    return discord.Webhook.from_url(os.environ["DISCORD_REBASE_BOT_WEBHOOK_URL"])
 
 
 def get_epoch_info():
