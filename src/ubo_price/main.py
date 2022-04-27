f732b52796c080e3c39194fbe9af16f91c8cff5e
Marcus Aurelius <marcus_aurelius_0x@protonmail.com>
2022-04-27 18:25:23 -0400

    Fix token price error handling

--- src/ubo_price/main.py
+++ src/ubo_price/main.py
@@ -28,13 +28,14 @@ async def on_ready():
 @tasks.loop(seconds=300)
 async def update_info():
     klima_price = klima_usdc_price(web3)
-    price = klima_price / uni_v2_pool_price(
+    token_price = uni_v2_pool_price(
         web3, UBO_KLIMA_POOL,
         KLIMA_DECIMALS - UBO_DECIMALS
     )
     supply = token_supply(web3, UBO_ADDRESS, ubo_abi, UBO_DECIMALS)
 
-    if price is not None and supply is not None:
+    if klima_price is not None and token_price is not None and supply is not None:
+        price = klima_price / token_price
         price_text = f'${price:,.2f} UBO'
 
         print(price_text)
