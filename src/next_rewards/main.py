c31ea07d8e2bcf0d6d5e5e0977ea9c1f0023f12c
Marcus Aurelius <marcus_aurelius_0x@protonmail.com>
2024-02-14 12:09:31 -0600

    Rename reward alerts bot

--- src/next_rewards/main.py
+++ src/next_rewards/main.py
@@ -14,7 +14,7 @@ from ..utils import get_discord_client, get_polygon_web3, load_abi, update_nickn
 BOT_TOKEN = os.environ["DISCORD_BOT_TOKEN"]
 SCAN_API_KEY = os.environ['POLYGONSCAN_API_KEY']
 
-REBASER_ROLE_ID = '912771496122916905'
+NOTIFY_ROLE_ID = '912771496122916905'
 
 # Initialized Discord client
 client = get_discord_client()
@@ -24,8 +24,8 @@ web3 = get_polygon_web3()
 
 staking_abi = load_abi('klima_staking.json')
 
-last_rebase_warning = 0
-last_rebase_alert = 0
+last_warning = 0
+last_alert = 0
 
 
 def get_webhook():
@@ -44,20 +44,20 @@ def get_epoch_info():
     return epoch_info
 
 
-def get_next_rebase_secs(next_rebase_block):
+def get_next_rewards_secs(next_rewards_block):
     '''Fetch the block countdown from PolygonScan'''
     resp = requests.get(
-        f'https://api.polygonscan.com/api?module=block&action=getblockcountdown&blockno={next_rebase_block}&apikey={SCAN_API_KEY}'  # noqa: E501
+        f'https://api.polygonscan.com/api?module=block&action=getblockcountdown&blockno={next_rewards_block}&apikey={SCAN_API_KEY}'  # noqa: E501
     )
 
     try:
-        next_rebase_secs = float(
+        next_rewards_secs = float(
             json.loads(resp.content)['result']['EstimateTimeInSec']
         )
     except (TypeError, json.decoder.JSONDecodeError):
-        next_rebase_secs = None
+        next_rewards_secs = None
 
-    return next_rebase_secs
+    return next_rewards_secs
 
 
 @client.event
@@ -69,8 +69,8 @@ async def on_ready():
 
 @tasks.loop(seconds=60)
 async def update_info():
-    global last_rebase_warning
-    global last_rebase_alert
+    global last_warning
+    global last_alert
     epoch_info = get_epoch_info()
 
     if epoch_info is None:
@@ -78,56 +78,56 @@ async def update_info():
 
     # unpack epoch info
     next_epoch_number = epoch_info[1]
-    next_rebase_block = epoch_info[2]
+    next_rewards_block = epoch_info[2]
 
     # Datetime calculations
-    next_rebase_secs = get_next_rebase_secs(next_rebase_block)
+    next_rewards_secs = get_next_rewards_secs(next_rewards_block)
 
-    if next_rebase_secs is None:
+    if next_rewards_secs is None:
         return
 
-    next_rebase_delta = timedelta(seconds=next_rebase_secs)
-    next_rebase_datetime = datetime.utcnow() + next_rebase_delta
+    next_rewards_delta = timedelta(seconds=next_rewards_secs)
+    next_rewards_datetime = datetime.utcnow() + next_rewards_delta
 
     # Extract hours and minutes from timedelta for formatting
-    hours, remainder = divmod(next_rebase_delta.total_seconds(), 3600)
+    hours, remainder = divmod(next_rewards_delta.total_seconds(), 3600)
     minutes, seconds = divmod(remainder, 60)
 
     # Format time
-    next_rebase_time = next_rebase_datetime.time().strftime("%H:%M")
+    next_rewards_time = next_rewards_datetime.time().strftime("%H:%M")
 
-    # More than 15m since the last rebase warning
+    # More than 15m since the last warning
     warning_mins = 15
     warning_secs = warning_mins * 60
-    if next_rebase_secs <= warning_secs and time() - last_rebase_warning > warning_secs + 30:
-        last_rebase_warning = time()
+    if next_rewards_secs <= warning_secs and time() - last_warning > warning_secs + 30:
+        last_warning = time()
         webhook = get_webhook()
         try:
             webhook.send(
-                f"Rebase imminent in approximately {warning_mins} minutes <@&{REBASER_ROLE_ID}>"
+                f"Rewards imminent in approximately {warning_mins} minutes <@&{NOTIFY_ROLE_ID}>"
             )
         except discord.errors.NotFound:
             print("Webhook not found")
 
-    # More than 120s since the last rebase alert
-    if next_rebase_secs <= 90 and time() - last_rebase_alert > 120:
-        last_rebase_alert = time()
+    # More than 120s since the last alert
+    if next_rewards_secs <= 90 and time() - last_alert > 120:
+        last_alert = time()
         webhook = get_webhook()
         try:
             webhook.send(
-                f"Rebasing momentarily! <@&{REBASER_ROLE_ID}> (:deciduous_tree:, :deciduous_tree:)"
+                f"Rewards distributed momentarily! <@&{NOTIFY_ROLE_ID}> (:deciduous_tree:, :deciduous_tree:)"
             )
         except discord.errors.NotFound:
             print("Webhook not found")
 
-    countdown_text = f'Rebase In: {int(hours)}h {int(minutes)}m'
+    countdown_text = f'Rewards in {int(hours)}h {int(minutes)}m'
 
     success = await update_nickname(client, countdown_text)
     if not success:
         return
 
     success = await update_presence(
-        client, f'Epoch {next_epoch_number} @ {next_rebase_time} UTC',
+        client, f'Epoch {next_epoch_number} @ {next_rewards_time} UTC',
         'playing'
     )
     if not success:
